f5caae2a713d14f3d129145e0cdf2feff8bf7ce5
syh0397 <syh34360397@gmail.com>
2022-09-01 01:20:35 +0900

    fastapi

--- Fastapi/src/main.py
+++ Fastapi/src/main.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , Body, Depends
 from pydantic import BaseModel, validator
 
 from utils.functions import calculate
+from utils import schemas
+from utils import models
+
+from database import Base, engine, SessionLocal
+from sqlalchemy.orm import Session 
+
+
+Base.metadata.create_all(engine)
+
+def get_session():
+    session = SessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
+
 
 class CalParam(BaseModel):
     cal_type: str
@@ -10,17 +26,32 @@ class CalParam(BaseModel):
 
     @validator('cal_type')
     def check_calculate_type(cls, v):
-        if v not in ['더하기', '빼기', '곱하기', '나누기']:
+        if v not in ['addition', 'subtraction', 'multiplication', 'division']:
             raise ValueError('cal_type must be one of "addition", "subtraction", "multiplication, or "division".')
         return v
 
     @validator('b')
     def check_zero_division(cls, v, values):
-        if values.get('cal_type') == '나누기' and v == 0:
+        if values.get('cal_type') == 'division' and v == 0:
             raise ValueError('b must not be zero.')
+        return v
+    
+    # a가 0이고 b가 10이면 안되는 함수를 만들어보자 
+    @validator('a')
+    def check_zero_addition(cls, v):
+        if  v == 10:
+            raise ValueError('a must not be zero.')
         return v
 
 app = FastAPI()
+
+
+fakeDatabase = {
+    1: {'task': 'Clean car'},
+    2: {'task': 'Clean my room'},
+    3: {'task': 'Clean my computer'},
+}
+
 
 # app은 server application 
 # @ 뒤에 app.get 과 url 경로를 입력한 후 함수를 만들면 바로 구현이 가능하다.
@@ -29,6 +60,54 @@ def read_root():
     return {
         'result': '설유환의 첫번째 Fast API 작품입니다.',
     }
+
+# 질의하는 함수 READ
+def getItems(session: Session = Depends(get_session)):
+    items = session.query(models.Item).all()
+    return items
+
+
+# ADD Data Create
+@app.get("/{id}")
+def getItem(id:int, session: Session = Depends(get_session)):
+    item = session.query(models.Item).get(id)
+    return item
+
+# @app.get('/getitem/')
+# def getItems():
+#     return ['Item 1', 'Item 2', 'Item 3']
+
+
+# ADD Data
+#Option #2
+@app.post("/")
+def addItem(item:schemas.Item, session: Session = Depends(get_session)):
+    item = models.Item(task = item.task)
+    session.add(item)
+    session.commit()
+    session.refresh(item)
+
+    return item
+
+
+# Update Data
+@app.put("/{id}")
+def updateItem(id:int, item:schemas.Item, session: Session = Depends(get_session)):
+    itemObject = session.query(models.Item).get(id)
+    itemObject.task = item.task
+    session.commit()
+    return itemObject
+
+# Deleting Data
+@app.delete("/{id}")
+def deleteItem(id:int, session: Session = Depends(get_session)):
+    itemObject = session.query(models.Item).get(id)
+    session.delete(itemObject)
+    session.commit()
+    session.close()
+    return 'Item was deleted...'
+
+
 
 
 @app.post('/calculator/')
--- Fastapi/src/utils/functions.py
+++ Fastapi/src/utils/functions.py
@@ -1,16 +1,14 @@
 
 def calculate(a, b, cal_type):
-    if param.cal_type == '더하기':
-        value = param.a + param.b
-    elif param.cal_type == '빼기':
-        value = param.a - param.b
-    elif param.cal_type == '곱하기':
-        value = param.a * param.b
-    elif param.cal_type == '나누기':
-        value = param.a / param.b
+    if cal_type == 'addition':
+        return a + b
+    elif cal_type == 'subtraction':
+        return a - b
+    elif cal_type == 'multiplication':
+        return a * b
+    elif cal_type == 'division':
+        return a / b
     else:
-        return {
-            'error': 'cal_type must be one of "더하기", "빼기", "곱하기, or "나누기".'}
-    return {'result': value}
+        raise ValueError('cal_type must be one of "addition", "subtraction", "multiplication, or "division".')
 
 # value error를 일으켜 HTTP status code를 400번대로 변경해야함 
